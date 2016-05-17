3c306d3265d26ab1e39bb88c4a072894604dee85
rabutler <r.alan.butler@gmail.com>
2016-05-16 18:55:30 -0600

    Add in additional read.rdf tests since cannot simply compare the two rdf lists on Travis.

--- tests/testthat/test_readrdf.R
+++ tests/testthat/test_readrdf.R
@@ -6,6 +6,17 @@ r1 <- read.rdf(system.file('extdata',file.path('Scenario','DNF,CT,IG','KeySlots.
 r2 <- read.rdf(system.file('extdata',file.path('Scenario','DNF,CT,IG','SystemConditions.rdf'),
                            package = 'RWDataPlot'))
 
+test_that('read.rdf data are as expected', {
+  expect_equal(rdfSlotToMatrix(r1, 'Powell.Pool Elevation'),
+               rdfSlotToMatrix(keyRdf, 'Powell.Pool Elevation'))
+  expect_equal(rdfSlotToMatrix(r1, 'Mead.Pool Elevation'),
+               rdfSlotToMatrix(keyRdf, 'Mead.Pool Elevation'))
+  expect_equal(rdfSlotToMatrix(r2, "SummaryOutputData.LBShortageConditions"),
+               rdfSlotToMatrix(sysRdf, "SummaryOutputData.LBShortageConditions"))
+  expect_equal(rdfSlotToMatrix(r2, "SummaryOutputData.MidElevationReleaseAt748"),
+               rdfSlotToMatrix(sysRdf, "SummaryOutputData.MidElevationReleaseAt748"))
+})
+
 exptSlots <- c("LBShort.AnnualMexicoShort", "LBShort.AnnualLBMexicoShort", "SummaryOutputData.LBNormalCondition",                    
   "SummaryOutputData.MidElevationReleaseAt823", "SummaryOutputData.LBShortageConditions", 
   "SummaryOutputData.UpperBalancingBelow823", "SummaryOutputData.LBSurplusConditions",                  
