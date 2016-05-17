48b171b79be91707a3c27d0494b46bf274e375fc
rabutler <r.alan.butler@gmail.com>
2016-05-17 15:03:54 -0600

    removed dummyFunction b/c it contains references to functions from other code and is not generic.

--- R/modifyDataTable.R
+++ R/modifyDataTable.R
@@ -12,35 +12,3 @@ changeDataFile <- function(iFile, varName, newVarName = varName, oldVals, newVal
   zz[,ii] = tmp
   utils::write.table(as.matrix(zz), iFile, row.names = F, sep = '\t')
 } 
-
-# todo: create general function for adding attributes based on arbitrary functions
-dummyFunction <- function()
-{
-  # tag for drought contingency modeling
-  if(tags == 'DCP'){
-    scenFull <- strsplit(sPath,'/')[[1]]
-    scenFull <- scenFull[length(scenFull)]
-    
-    dryOrWet15 <- readDryWetFile(scenFull, '15To19')
-    dryOrWet20 <-readDryWetFile(scenFull, '20To26')
-    vulnT <- readVulnFile(scenFull)
-    d15 <- match(allRes$Trace, names(dryOrWet15))
-    d15 <- dryOrWet15[d15]
-    d20 <- match(allRes$Trace, names(dryOrWet20))
-    d20 <- dryOrWet20[d20]
-    vv <- match(allRes$Trace, names(vulnT))
-    vv <- vulnT[vv]
-    
-    allRes$DryOrWet2015To2019 <- simplify2array(d15)
-    allRes$DryOrWet2020To2026 <- simplify2array(d20)
-    allRes$VulnTrace <- simplify2array(vv)
-    
-    # create a separate column for supply, one for Scenario, and one for hyd-Scenario
-    tmp <- as.character(allRes$Scenario)
-    allRes$Hyd_Scen <- allRes$Scenario
-    onlyScen <- getScenNoSupply(scenFull)
-    onlyHyd <- getScenHydrology(scenFull)
-    allRes$Scenario <- rep(onlyScen, nrow(allRes))
-    allRes$Hydrology <- rep(onlyHyd, nrow(allRes))
-  }
-}
