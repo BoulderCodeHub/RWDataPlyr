da8be4a56e58508e50321bda459b001a42737488
rabutler <r.alan.butler@gmail.com>
2016-07-08 11:57:34 -0600

    added file extension test for getDataForAllScens

--- tests/testthat/test_getDataForAllScens.R
+++ tests/testthat/test_getDataForAllScens.R
@@ -26,3 +26,16 @@ test_that("getting all slot data is the same as using a pre-configured slotAggLi
   expect_equal(dplyr::filter(keyData, Variable == 'Mead.Pool Elevation_EOCY_1')$Value,
              dplyr::filter(allData, Variable == 'Mead.Pool Elevation_Monthly_1', Month == 'Dec')$Value)
 })
+
+test_that('file extension is checked', {
+  expect_error(getDataForAllScens(scenFolders, scenNames, slotAggList, scenPath, 
+                                  'tst.xyz', retFile),
+               paste0('oFile has an invalid file exention.\n',
+                       'getDataForAllScens does not know how to handle ".', 'xyz',
+                       '" extensions.'))
+  expect_error(getDataForAllScens(scenFolders, scenNames, slotAggList, scenPath, 
+                                  'tst.cvs', retFile),
+               paste0('oFile has an invalid file exention.\n',
+                      'getDataForAllScens does not know how to handle ".', 'cvs',
+                      '" extensions.'))
+})
