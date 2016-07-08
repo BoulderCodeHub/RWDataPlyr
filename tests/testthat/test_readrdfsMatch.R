767046928cb836de915cdd9d23f9170172c46ee1
rabutler <r.alan.butler@gmail.com>
2016-07-08 11:21:53 -0600

    checking individual components of list instead of entire list.

--- tests/testthat/test_readrdfsMatch.R
+++ tests/testthat/test_readrdfsMatch.R
@@ -11,11 +11,15 @@ r3 <- read.rdf2(system.file('extdata',file.path('Scenario','DNF,CT,IG','KeySlots
 r4 <- read.rdf2(system.file('extdata',file.path('Scenario','DNF,CT,IG','SystemConditions.rdf'),
                            package = 'RWDataPlot'))
 
-test_that('lists are identical', {
-  expect_true(all.equal(r1,r3))
-  expect_true(all.equal(r2,r4))
-  expect_equivalent(r1, r3)
-  expect_equivalent(r2, r4)
+test_that('list components match', {
+  expect_equal(r1$meta, r3$meta)
+  expect_equal(length(r1$runs), length(r1$runs))
+  expect_equal(names(r1$runs[[1]]), names(r3$runs[[1]]))
+  expect_equal(r1$runs[[1]]$start, r3$runs[[1]]$start)
+  expect_equal(r1$runs[[1]]$end, r3$runs[[1]]$end)
+  expect_equal(names(r1$runs[[1]]$objects), names(r3$runs[[1]]$objects))
+  expect_equal(r1$runs[[1]]$objects[['Mead.Pool Elevation']]$values,
+               r3$runs[[1]]$objects[['Mead.Pool Elevation']]$values)
 })
 
 test_that('getting data matches', {
