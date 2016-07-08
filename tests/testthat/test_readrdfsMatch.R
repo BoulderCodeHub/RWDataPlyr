4488d61741c76d05c89f9e3bf228cc1974e56555
rabutler <r.alan.butler@gmail.com>
2016-07-08 10:06:28 -0600

    another test to see if tests can pass on Travis

--- tests/testthat/test_readrdfsMatch.R
+++ tests/testthat/test_readrdfsMatch.R
@@ -3,17 +3,21 @@ context('check read.rdf2 matches read.rdf')
 
 r1 <- read.rdf(system.file('extdata',file.path('Scenario','DNF,CT,IG','KeySlots.rdf'),
                            package = 'RWDataPlot'))
+r1.1 <- read.rdf(system.file('extdata',file.path('Scenario','DNF,CT,IG','KeySlots.rdf'),
+                           package = 'RWDataPlot'))
 r2 <- read.rdf(system.file('extdata',file.path('Scenario','DNF,CT,IG','SystemConditions.rdf'),
                            package = 'RWDataPlot'))
 
 r3 <- read.rdf2(system.file('extdata',file.path('Scenario','DNF,CT,IG','KeySlots.rdf'),
                            package = 'RWDataPlot'))
+r3.1 <- read.rdf2(system.file('extdata',file.path('Scenario','DNF,CT,IG','KeySlots.rdf'),
+                            package = 'RWDataPlot'))
 r4 <- read.rdf2(system.file('extdata',file.path('Scenario','DNF,CT,IG','SystemConditions.rdf'),
                            package = 'RWDataPlot'))
 
 test_that('lists are identical', {
-  expect_equal(r1,r1)
-  expect_equal(r2,r2)
+  expect_equal(r1,r1.1)
+  expect_equal(r3,r3.1)
 })
 
 test_that('getting data matches', {
