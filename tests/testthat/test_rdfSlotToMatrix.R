c8331da2b771a0c1cf884d4954010ff15fe95c08
Alan Butler <rabutler@usbr.gov>
2016-10-19 16:58:42 -0600

    Added the test back in that checks dimensions returned from `rdfSlotToMatrix` since SysRdf was updated.

--- tests/testthat/test_rdfSlotToMatrix.R
+++ tests/testthat/test_rdfSlotToMatrix.R
@@ -2,6 +2,6 @@ library(RWDataPlot)
 context('check rdfSlotToMatrix')
 
 test_that("rdfSlotToMatrix gets correct dimensions for matrix", {
-  expect_equal(dim(rdfSlotToMatrix(keyRdf,'Powell.Pool Elevation')),c(132,25))
-  #expect_equal(dim(rdfSlotToMatrix(sysRdf,'SummaryOutputData.LBShortageConditions')),c(11,25))
+  expect_equal(dim(rdfSlotToMatrix(keyRdf,'Powell.Pool Elevation')),c(240,25))
+  expect_equal(dim(rdfSlotToMatrix(sysRdf,'SummaryOutputData.LBShortageConditions')),c(20,25))
 })
