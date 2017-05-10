a3eb8ea007bdd7abe5c57e7ee5ef3edc4ec781af
Alan Butler <rabutler@usbr.gov>
2017-05-10 17:56:42 -0600

    added more tests to check the format of the list returned by `createSlotAggList`

--- tests/testthat/test_createSlotAggList.R
+++ tests/testthat/test_createSlotAggList.R
@@ -21,3 +21,36 @@ test_that("createSlotAggList returns proper errors", {
   expect_error(createSlotAggList(file.path("some", "crazy", "file.txt")),
                paste(file.path("some", "crazy", "file.txt"),'does not exist.'))
 })
+
+
+sal <- createSlotAggList(system.file("extdata", "SlotAggTable.csv", package = "RWDataPlyr"))
+sam <- matrix(c(
+  "KeySlots.rdf", "Mead.Pool Elevation", "EOCY", NA, "meadPe",
+  "SystemConditions.rdf", "Shortage.ShortageFlag", "AnnualRaw", 100, "lbShort",
+  "KeySlots.rdf", "Powell.Pool Elevation", "EOCY", NA, "powellPe"
+), ncol = 5, byrow = TRUE)
+sal2 <- createSlotAggList(sam)
+
+test_that("format of createSlotAggList is correct", {
+  expect_equal(length(sal), 1)
+  expect_equal(length(sal[[1]]$rdf), 1)
+  expect_equal(length(sal[[1]]$slots), 4)
+  expect_equal(nrow(sal[[1]]$annualize), 2)
+  expect_equal(ncol(sal[[1]]$annualize), 4)
+  expect_equal(length(sal[[1]]$varNames), 4)
+  expect_true(all(is.na(sal[[1]]$varNames)))
+  
+  expect_equal(length(sal2), 2)
+  expect_equal(length(sal2[[1]]$rdf), 1)
+  expect_equal(length(sal2[[1]]$slots), 2)
+  expect_equal(nrow(sal2[[1]]$annualize), 2)
+  expect_equal(ncol(sal2[[1]]$annualize), 2)
+  expect_equal(length(sal2[[1]]$varNames), 2)
+  expect_false(all(is.na(sal2[[1]]$varNames)))
+  expect_equal(length(sal2[[2]]$rdf), 1)
+  expect_equal(length(sal2[[2]]$slots), 1)
+  expect_equal(nrow(sal2[[2]]$annualize), 2)
+  expect_equal(ncol(sal2[[2]]$annualize), 1)
+  expect_equal(length(sal2[[2]]$varNames), 1)
+  expect_false(all(is.na(sal2[[2]]$varNames)))
+})
