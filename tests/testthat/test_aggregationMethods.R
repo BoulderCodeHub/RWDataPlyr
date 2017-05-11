86ef0984e1c0b121b29504816768eb4509dbcae8
Alan Butler <rabutler@usbr.gov>
2017-05-10 18:44:35 -0600

    initial test for `processSlots` aggregation methods

---
+++ tests/testthat/test_aggregationMethods.R
@@ -0,0 +1,41 @@
+library(RWDataPlyr)
+library(dplyr)
+library(tidyr)
+
+context('check RWDataPlyr:::processSlots aggregation computations')
+
+keyRdf <- read.rdf2(system.file('extdata/Scenario/DNF,CT,IG', "KeySlots.rdf", 
+                                package = "RWDataPlyr"))
+mEocy <- rdfSlotToMatrix(keyRdf, "Mead.Pool Elevation")
+mEocy <- as.data.frame(mEocy[seq(12,nrow(mEocy), 12),])
+colnames(mEocy) <- 1:ncol(mEocy)
+
+
+# **** create a sam that tests every one of the agg methods
+sal <- createSlotAggList(matrix(c(
+  "KeySlots.Rdf", "Mead.Pool Elevation", "EOCY", NA, "meadPe"
+), ncol = 5))
+
+zz <- getDataForAllScens(
+  scenFolders = "DNF,CT,IG", 
+  scenNames = "DNF,CT,IG", 
+  slotAggList = sal, 
+  scenPath = system.file('extdata','Scenario/',package = 'RWDataPlyr'),
+  oFile = "tmp.feather",
+  retFile = TRUE
+)
+
+test_that("processSlots monthly to annual aggregation methods work", {
+  expect_equal({
+    zz %>% 
+      filter(Variable == "meadPe", Scenario == "DNF,CT,IG") %>%
+      select(-Scenario, -Variable) %>%
+      arrange(Year) %>%
+      spread(Trace, Value) %>%
+      select(-Year)
+  }, mEocy)
+})
+
+# **** also need to check the scaling
+
+on.exit(file.remove("tmp.feather"))
