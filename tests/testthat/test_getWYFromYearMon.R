67d06755e2d7f84a05aad859011391d543843f2c
Alan Butler <rabutler@usbr.gov>
2017-05-26 10:39:16 -0600

    improving tests of `getWYFromYearMon()`

--- tests/testthat/test_getWYFromYearMon.R
+++ tests/testthat/test_getWYFromYearMon.R
@@ -1,6 +1,7 @@
 library(RWDataPlyr)
 library(zoo)
 
+
 context("check getWYFromYearMon")
 
 test_that("dimensions are correct", {
@@ -15,4 +16,15 @@ test_that("WY is computed as expected", {
                                 c(2000, 2000, 2001))
   expect_equal(getWYFromYearmon(as.yearmon(2000 + seq(0, 23)/12)), 
                c(rep(2000, 9), rep(2001, 12), rep(2002, 3)))
+})
+
+test_that("warnings and errors post correctly", {
+  expect_warning(getWYFromYearmon("2000-11"), 
+                 "ym, is not a yearmon object. attempting to convert to yearmon...")
+  expect_warning(
+    getWYFromYearmon("Feb 1977"),
+    "ym, is not a yearmon object. attempting to convert to yearmon..."
+  )
+  expect_warning(expect_error(getWYFromYearmon("abc"), "could not convert ym to yearmon"))
+  expect_warning(expect_error(getWYFromYearmon("20000118"), "could not convert ym to yearmon"))
 })
