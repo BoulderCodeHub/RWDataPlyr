e9dc5739286a0613bf6b664811f9b59642cb7ee1
Alan Butler <rabutler@usbr.gov>
2018-02-02 17:18:30 -0700

    added `keep_cols` parameter to `rdf_trace_to_tbl()`
    This lets the user specify which columns are returned by this function.
    
    working on #30

--- R/rdf_to_tbl.R
+++ R/rdf_to_tbl.R
@@ -71,6 +71,16 @@ rdf_trace_to_tbl <- function(rdfTrace, traceNum)
 #' @param scenario An optional parameter, that if it is not `NULL` (default)
 #'   will be added to the tibble as another variable. Typically a string, but it 
 #'   is not coerced to a string.
+#' @param keep_cols Either boolean, or a character vector of column names to 
+#'   keep in the returned tibble. The "Timestep", "TraceNumber", "ObjectSlot", 
+#'   and "Value" columns are always returned. `keep_cols` allows 
+#'   the user to include other columns that are not always required, e.g., 
+#'   "Object" and "Slot" instead of only the "ObjectSlot" column. If `keep_cols`
+#'   is `FALSE`, then only the above 5 columns are returned. If `keep_cols` is
+#'   `TRUE`, then all columns are returned. Finally, if `keep_cols` is a 
+#'   character vector, then the above 5 columns are returned, along with any of
+#'   those specified in `keep_cols`; if they are not found, a warning will post.
+#'   If `scenario` is specified, it will also always be returned.
 #' 
 #' @return A tibble with additional attributes from the rdf list
 #' 
@@ -82,8 +92,13 @@ rdf_trace_to_tbl <- function(rdfTrace, traceNum)
 #' 
 #' @export
 
-rw_rdf_to_tbl <- function(rdf, scenario = NULL)
+rw_rdf_to_tbl <- function(rdf, scenario = NULL, keep_cols = FALSE)
 {
+  stopifnot(
+    (is.logical(keep_cols) && !is.na(keep_cols) && length(keep_cols == 1)) || 
+    (is.character(keep_cols) && length(keep_cols) > 0)
+  )
+  
   # rdf[["meta"]] contains meta data
   # rdf[["runs"]] will be the length of rdf[[1]]$number_of_runs
   atts <- rdf$meta
@@ -94,15 +109,54 @@ rw_rdf_to_tbl <- function(rdf, scenario = NULL)
     lapply(1:nRun, function(x) rdf_trace_to_tbl(rdf[[2]][[x]], traceNum = x))
   )
   
-  if(!is.null(scenario))
+  if ((is.logical(keep_cols) && !keep_cols) || is.character(keep_cols))
+    tbl <- select_rdftbl_cols(tbl, keep_cols)
+  
+  if (!is.null(scenario))
     tbl$Scenario <- scenario
   
   structure(
     tibble::as_tibble(tbl),
-    "MRMName" = atts$name,
+    "mrm_config_name" = atts$name,
     "owner" = atts$owner,
     "description" = atts$description,
-    "createDate" = atts$create_date,
-    "numberOfTraces" = atts$numer_of_runs
+    "create_date" = atts$create_date,
+    "n_traces" = nRun
   )
 }
+
+#' Standard (required) Column Names for RDF Tables that will always be returned
+#' by `rw_rdf_to_tbl()`
+#' 
+#' @noRd
+
+req_rwtbl_cols <- function()
+{
+  c("Timestep","TraceNumber", "ObjectSlot", "Value")
+}
+
+#' Select a subset of the columns in the rdftbl, as specified by `keep_cols`
+#' 
+#' @noRd
+
+select_rdftbl_cols <- function(rdftbl, keep_cols)
+{
+  if (is.logical(keep_cols))
+  {
+    select_cols <- req_rwtbl_cols()
+  } else {
+    # check that the user specified keep_cols are actually columns in rdftbl
+    colsInTbl <- (keep_cols %in% colnames(rdftbl))
+    missing_cols <- keep_cols[!colsInTbl]
+    if (length(missing_cols) > 0)
+      warning(
+        "The following columns specified by 'keep_cols' were not found in the rwtbl:\n",
+        "    ", toString(missing_cols)
+      )
+    select_cols <- c(req_rwtbl_cols(), keep_cols[colsInTbl])
+  }
+  
+  rdftbl %>%
+    dplyr::select_at(.vars = select_cols)
+}
+
