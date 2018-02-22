0ee2457f0e7bcdabe079c1421d79c53b6ae6ecef
Alan Butler <rabutler@usbr.gov>
2018-02-22 16:16:37 -0700

    moved all file and path checking in `rw_scen_aggregate()` to independent functions

--- R/rw_scen_aggregate.R
+++ R/rw_scen_aggregate.R
@@ -20,11 +20,48 @@ rw_scen_aggregate <- function(scenarios,
   if (length(scen_dir) != 1) {
     stop("`scen_dir` should only have a length of 1.", call. = FALSE)
   }
-  
   scen_dir <- normalizePath(scen_dir, mustWork = TRUE)
   
   nans_are <- match.arg(nans_are, choices = c("0", "error"))
   
+  check_scen_rdf_paths(scenarios, scen_dir, agg)
+  
+  # check that file is correct type if it's specified
+  if (!missing(file)) {
+    check_rw_agg_file(file)
+  }
+
+  scenarios <- get_scen_names(scenarios, scen_names)
+}
+
+get_scen_names <- function(scenarios, scen_names)
+{
+  # if scenarios have names, then scen_names should not be specified
+  if (!is.null(names(scenarios)) && !missing(scen_names)) {
+    stop(
+      "In `rw_scen_aggregate()`, `scenarios` have `names()`, so `scen_names` should not be specified.",
+      call. = FALSE
+    )
+  }
+  
+  if (is.null(names(scenarios)) && !missing(scen_names)) {
+    names(scenarios) <- scen_names
+  }
+  
+  if (!is.null(names(scenarios))) {
+    if (any(names(scenarios) %in% "")) {
+      repI <- which(names(scenarios) %in% "")
+      names(scenarios)[repI] <- scenarios[repI]
+    }
+  } else if (is.null(names(scenarios)) && missing(scen_names)) {
+    names(scenarios) <- scenarios
+  }
+  
+  scenarios
+}
+
+check_scen_rdf_paths <- function(scenarios, scen_dir, agg)
+{
   # check all scenario paths
   scen_paths <- file.path(scen_dir, scenarios)
   if (! all(dir.exists(scen_paths))) {
@@ -46,43 +83,24 @@ rw_scen_aggregate <- function(scenarios,
       call. = FALSE
     )
   }
-  
-  # check that file is correct type if it's specified
-  if (!missing(file)) {
-    if (length(file) != 1) {
-      stop("In `rw_scen_aggregate()`, `file` should have a length of 1.")
-    }
-    
-    if (!(tools::file_ext(file) %in% c("csv", "feather", "txt"))) {
-      stop(
-        "In `rw_scen_aggregate()`, `file` should have a .csv, .feather, or .txt extension.", 
-        call. = FALSE
-      )
-    }
-    
-    if (!dir.exists(dirname(file))) {
-      stop("In `rw_scen_aggregate()`, `file` should point to a valid location.")
-    }
+}
+
+check_rw_agg_file <- function(file)
+{
+  if (length(file) != 1) {
+    stop("In `rw_scen_aggregate()`, `file` should have a length of 1.")
   }
   
-  # if scenarios have names, then scen_names should not be specified
-  if (!is.null(names(scenarios)) && !missing(scen_names)) {
+  if (!(tools::file_ext(file) %in% c("csv", "feather", "txt"))) {
     stop(
-      "In `rw_scen_aggregate()`, `scenarios` have `names()`, so `scen_names` should not be specified.",
+      "In `rw_scen_aggregate()`, `file` should have a .csv, .feather, or .txt extension.", 
       call. = FALSE
     )
   }
   
-  if (is.null(names(scenarios)) && !missing(scen_names)) {
-    names(scenarios) <- scen_names
+  if (!dir.exists(dirname(file))) {
+    stop("In `rw_scen_aggregate()`, `file` should point to a valid location.")
   }
   
-  if (!is.null(names(scenarios))) {
-    if (any(names(scenarios) %in% "")) {
-      repI <- which(names(scenarios) %in% "")
-      names(scenarios)[repI] <- scenarios[repI]
-    }
-  } else if (is.null(names(scenarios)) && missing(scen_names)) {
-    names(scenarios) <- scenarios
-  }
+  invisible(file)
 }
