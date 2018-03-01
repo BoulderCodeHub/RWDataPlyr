fb200be0aae77bfa152966dc21691d9b33dae73e
Alan Butler <rabutler@usbr.gov>
2018-02-28 18:02:53 -0700

    updated the `wy()` function so that it drops data if there are less than a specified number of months

--- R/period_funs.R
+++ R/period_funs.R
@@ -9,13 +9,29 @@ wy <- function()
 {
   wy_convert <- function(rwtbl)
   {
-    rwtbl %>%
+    tmp <- rwtbl %>%
       dplyr::mutate_at(
         "Timestep", 
         .funs = dplyr::funs("ym" = zoo::as.yearmon)
       ) %>%
       dplyr::mutate_at("ym", .funs = dplyr::funs("Year" = getWYFromYearmon)) %>%
       dplyr::select(-dplyr::one_of("ym"))
+    
+    # drop if WY contains less than 6 months of data for the year
+    cols <- names(tmp)
+    cols <- cols[!(cols %in% c("Timestep", "Month", "Value"))]
+    keep_yrs <- unique(
+      (tmp %>% 
+         dplyr::group_by_at(cols) %>% 
+         dplyr::tally() %>% 
+         dplyr::filter_at(
+           "n", 
+           dplyr::any_vars(. > getOption("rwdataplyr.wy_month_tol"))
+         )
+      )$Year
+    )
+    
+    dplyr::filter_at(tmp, "Year", dplyr::any_vars(. %in% keep_yrs))
   }
   
   list(fun = wy_convert, filter_months = month.name, group_tbl = c("Year"))
