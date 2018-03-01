c96d45b7be2d2721590218a2959fed70ac1d2337
Alan Butler <rabutler@usbr.gov>
2018-03-01 12:51:32 -0700

    New function `rwtbl_var_to_slot()` for #50 with documentation and tests

--- R/rwtbl_helpers.R
+++ R/rwtbl_helpers.R
@@ -30,3 +30,51 @@ rwtbl_slot_names <- function(rwtbl)
   
   as.character(levels(as.factor(rwtbl[[varname]])))
 }
+
+#' Map a variable name to the RiverWare slot name
+#' 
+#' `rwtbl_var_to_slot()` provides the RiverWare slot name that was used to 
+#' create the specified variable name (`varname`). If `varname` is not found in
+#' `rwtblsmmry`, a warning message is posted.
+#' 
+#' @param rwtblsmmry A tbl_df of summarizes RiverWare data; likely output from
+#'   [rw_scen_aggregate()].
+#' 
+#' @param varname A vector of variable names to map to slot names.
+#' 
+#' @return A character vector of the found slot names. `character(0)` if no 
+#'   variable names were found.
+#'   
+#' @examples 
+#' rwtbl_var_to_slot(scen_data, "peLt1000")
+#' rwtbl_var_to_slot(scen_data, c("peLt1000", "peEocy"))
+#' 
+#' @export
+rwtbl_var_to_slot <- function(rwtblsmmry, varname)
+{
+  if (!("Variable" %in% names(rwtblsmmry))) {
+    stop("Invalid `tbl_df` passed to `rwtbl_var_to_slot()`.\n",
+         "It does not have a `Variable` column")
+  } 
+  
+  rwa <- attr(rwtblsmmry, "rwd_agg")
+  
+  if (is.null(rwa))
+  {
+    stop("Invalid `tbl_df` passed to `rwtbl_var_to_slot()`.\n",
+         "It does not have a `rwd_agg` attribute.")
+  }
+  
+  slot_names <- rwa$slot[match(varname, rwa$variable)]
+  
+  if (anyNA(slot_names)) {
+    tmp <- varname[is.na(slot_names)]
+    warning("The following variables were not found in the rwtbl:\n",
+            paste(paste("    ", tmp), collapse = "\n"),
+            call. = FALSE)
+    
+    slot_names <- slot_names[!is.na(slot_names)]
+  }
+  
+  slot_names
+}
--- tests/testthat/test_rwtbl_helpers.R
+++ tests/testthat/test_rwtbl_helpers.R
@@ -1,3 +1,5 @@
+library(dplyr)
+# rwtbl_slot_names() ---------------------
 context("Test `rwtbl_slot_names()` function")
 
 rwtbl <- rw_rdf_to_tbl(keyRdf)
@@ -36,4 +38,56 @@ test_that("`rwtbl_slot_names()` fails for other data frames", {
     errMsg,
     fixed = TRUE
   )
+})
+
+# rwtbl_var_to_slot() ---------------------
+context("Test `rwtbl_var_to_slot()`")
+
+test_that("`rwtbl_var_to_slot()` errors correctly", {
+  expect_error(
+    rwtbl_var_to_slot(mtcars, "peLt1000"), 
+    paste0("Invalid `tbl_df` passed to `rwtbl_var_to_slot()`.\n",
+           "It does not have a `Variable` column"),
+    fixed = TRUE
+  )
+  expect_error(
+    rwtbl_var_to_slot(mutate(mtcars, Variable = rownames(mtcars)), "peLt1000"),
+    paste0("Invalid `tbl_df` passed to `rwtbl_var_to_slot()`.\n",
+           "It does not have a `rwd_agg` attribute."),
+    fixed = TRUE
+  )
+})
+rwa <- rwd_agg(read.csv(
+  system.file("extdata/rwd_agg_files/passing_aggs.csv", package = "RWDataPlyr"), 
+  stringsAsFactors = FALSE)
+)
+test_that("`rwtbl_var_to_slot()` works", {
+  expect_type(tmp <- rwtbl_var_to_slot(scen_data, rwa$variable), "character")
+  expect_length(tmp, nrow(rwa))
+  expect_setequal(tmp, rwa$slot)
+  
+  expect_type(tmp <- rwtbl_var_to_slot(scen_data, rwa$variable[1]), "character")
+  expect_length(tmp, 1)
+  expect_setequal(tmp, rwa$slot[1])
+  
+  expect_type(
+    expect_warning(
+      tmp <- rwtbl_var_to_slot(scen_data, c(rwa$variable[1:2], "okey")), 
+      paste0("The following variables were not found in the rwtbl:\n",
+             paste(paste("    ", "okey"), collapse = "\n"))
+    ),
+    "character"
+  )
+  expect_length(tmp, 2)
+  expect_setequal(tmp, rwa$slot[1:2])
+  
+  expect_type(
+    expect_warning(
+      tmp <- rwtbl_var_to_slot(scen_data, c("noway", "okey")), 
+      paste0("The following variables were not found in the rwtbl:\n",
+             paste(paste("    ", c("noway", "okey")), collapse = "\n"))
+    ),
+    "character"
+  )
+  expect_length(tmp, 0)
 })
