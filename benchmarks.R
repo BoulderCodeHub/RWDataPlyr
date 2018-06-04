2188a4ecaae0b8229b1344f753a0a1aa3093e03c
Alan Butler <rabutler@usbr.gov>
2018-06-04 15:16:39 -0600

    updating benchmarks

--- benchmarks.R
+++ benchmarks.R
@@ -8,21 +8,22 @@ rdf_vec <- as.matrix(data.table::fread(
 
 rdf <- read_rdf("inst/extdata/Scenario/ISM1988_2014,2007Dems,IG,Most/KeySlots.rdf")
 
+std_cols <- c("Timestep", "TraceNumber", "ObjectSlot", "Value")
 
 mb <- microbenchmark::microbenchmark(
-  "c++" = RWDataPlyr:::rdf_to_rwtbl_cpp(rdf_vec) %>% tibble::as_tibble(),
+  "c++" = RWDataPlyr:::rdf_to_rwtbl_cpp(rdf_vec, keep_cols = std_cols, add_ym = FALSE) %>% tibble::as_tibble(),
   "c++ old" = RWDataPlyr:::rdf_to_rwtbl_cpp_old(rdf_vec) %>% tibble::as_tibble(),
   "R" = rdf_to_rwtbl(rdf, keep_cols = TRUE),
   times = 50
 )
 
 mb2<- microbenchmark::microbenchmark(
-  "c++" = RWDataPlyr:::rdf_to_rwtbl_cpp(rdf_vec, scenario = "haha") %>% tibble::as_tibble(),
+  "c++" = RWDataPlyr:::rdf_to_rwtbl_cpp(rdf_vec, scenario = "haha", keep_cols = std_cols) %>% tibble::as_tibble(),
   "c++ old" = RWDataPlyr:::rdf_to_rwtbl_cpp_old(rdf_vec) %>% tibble::as_tibble(),
   "R" = rdf_to_rwtbl(rdf, keep_cols = TRUE, scenario = "haha"),
   times = 50
 )
 
 
-t1 <- RWDataPlyr:::rdf_to_rwtbl_cpp(rdf_vec, scenario = "haha") %>% tibble::as_tibble()
+t1 <- RWDataPlyr:::rdf_to_rwtbl_cpp(rdf_vec, scenario = "haha", keep_cols = std_cols) %>% tibble::as_tibble()
 t2 <- rdf_to_rwtbl(rdf, keep_cols = TRUE, scenario = "haha")
